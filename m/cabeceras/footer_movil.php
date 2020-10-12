6c945ccda3ca859aafa7c70456ba3b0a38ff039f
UlisesPA <ulises-96_@hotmail.com>
2020-10-11 21:05:15 -0500

    Update seccion footer

--- m/cabeceras/footer_movil.php
+++ m/cabeceras/footer_movil.php
@@ -1,5 +1,74 @@
 <section>
-	<footer>
-		
-	</footer>
+	
+		<div class="fondo_footer_movil"> </div>
+
+		<div class="footer_movil_content">
+						<div class="up_box">
+							<h1 class="titulo_footer_movil">bintech</h1>
+							<div class="content">
+								<h3 class="subtitulo_footer_movil">México</h3>
+								<div class="place">
+									<span class="fas fa-map-marker-alt"></span>
+								<span class="footer_movil_text">Av. Barco Viejo 4750<br>Oficina 2506 Torre Exertia<br>Boca del Río, Ver. 94290</span>
+								</div><br>
+								<div class="phone">
+									<span class="fas fa-mobile"></span>
+								<span class="footer_movil_text"> Tel.: +52 (229) 167 6798</span>
+								</div>
+
+								<div class="email">
+									<span class="fas fa-envelope"></span>
+								<span class="footer_movil_text">info@bintech.com.mx</span>
+								</div>
+
+								<div class="place">
+									<span class="fas fa-map-marker-alt"></span>
+								<span class="footer_movil_text">Av. Santa Fe 94 Tore A. Piso 8 Samara Tower Santa Fe, CDMX. 01210</span>
+								</div>
+
+								<div class="phone">
+									<span class="fas fa-mobile"></span>
+								<span class="footer_movil_text"> Tel.: +52 (55) 8851 1211</span>
+								</div>
+
+							</div>
+						</div>
+
+						<div class="middle_box">
+							<h3 class="subtitulo_footer_movil">Estados Unidos</h3>
+							<div class="place">
+									<span class="fas fa-map-marker-alt"></span>
+								<span class="footer_movil_text">2800 Post Oak Blvd. Suite 1400. Huston, TX. 77056</span>
+								</div>
+
+								<div class="phone">
+									<span class="fas fa-mobile"></span>
+								<span class="footer_movil_text"> Tel: +52 (229) 260 43 59</span>
+								</div>
+
+								<div class="email">
+									<span class="fas fa-envelope"></span>
+								<span class="footer_movil_text">info@bintech.com.mx</span>
+								</div>
+						</div>
+
+	<!-- 					<div class="right box">
+						<h3 class="subtitulo_footer_movil">Contacto</h3>
+						<form action="#">
+							<div class="email">
+								<div class="footer_movil_text">Email *</div>
+								<input class="input_email" type="email" required>
+  								<div role="button" class="btn_footer">
+									<button>enviar</button>
+								</div> 
+							</div>
+
+							<div class="message">
+								<div class="footer_movil_text">Mensaje *</div>
+								<textarea rows="4" cols="40" required></textarea>
+							</div>
+						</form>
+						</div>
+					</div> -->
+	
 </section>
