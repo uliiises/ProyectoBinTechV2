1a718ca8f8ae364f1ad874a7a4b14a41b2425639
UlisesPA <ulises-96_@hotmail.com>
2020-10-12 22:43:44 -0500

    Servicios Movil

---
+++ m/servicios.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Antonio Amador Barrientos / Uises Ponce Ávila">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<script src="https://kit.fontawesome.com/abdc980b7b.js" crossorigin="anonymous"></script>
+	<script src="../library/Jquery/jquery-3.5.1.min.js"></script>
+	<script src="../library/Jquery/jquery.onepage-scroll.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="../css/librerias_css/animate.css">
+	<link rel="stylesheet" type="text/css" href="../css/header.css">
+	<link rel="stylesheet" type="text/css" href="../css/footer.css">
+	<link rel="stylesheet" type="text/css" href="../css/landing_page.css">
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+
+
+	<title>Bintech - Servicios</title>
+</head>
+<body>
+	<?php require_once "cabeceras/header_movil.php"; ?>
+
+	<img class="landing_page_background" src="../img/industrias/section1/industrias_sec1_background.png">
+
+			<img class="sec1_capa_4_movil" src="../img/landing_page/sec1_capa_4_movil.png">
+
+			<h1 class="titulo_landing">Servicios</h1>
+
+			<div class="texto_landing">
+				<p>Estamos interesados en conocer tu empresa para ofrecerte la mejor solución</p>
+			</div>
+
+			<?php require_once "../cabeceras/redes_sociales.php"; ?>
+
+			<div class="maintext_industria">
+				<div class="text_content">
+					<h1 class="industrias_sec2_titulo">Industrias<br>a las que<br>atendemos</h1>
+					<div class="industrias_sec2_texto">
+						<p>En Bintech hemos asesorado a instituciones de salud públicas y privadas en diversos países. Somos pioneros en la institunacionalización de modelos financieros de rentabilidad, gestion de costos y planeación de vinculados.</p>
+					</div>
+
+	</body>
+</html>
