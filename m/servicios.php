dc69165a3b7cee5ff8967ed27ef1bbd5a0bae975
Jose Ulises <48640555+uliiises@users.noreply.github.com>
2020-10-18 20:02:57 -0500

    Add files via upload

--- m/servicios.php
+++ m/servicios.php
@@ -5,63 +5,153 @@
 	<meta charset="utf-8">
 	<meta name="author" content="Antonio Amador Barrientos / Uises Ponce Ávila">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
 
 	<script src="https://kit.fontawesome.com/abdc980b7b.js" crossorigin="anonymous"></script>
 	<script src="../library/Jquery/jquery-3.5.1.min.js"></script>
 	<script src="../library/Jquery/jquery.onepage-scroll.js"></script>
 
+	<link rel="stylesheet" type="text/css" href="../css/librerias_css/onepage-scroll.css">
 	<link rel="stylesheet" type="text/css" href="../css/librerias_css/animate.css">
 	<link rel="stylesheet" type="text/css" href="../css/header.css">
 	<link rel="stylesheet" type="text/css" href="../css/footer.css">
 	<link rel="stylesheet" type="text/css" href="../css/landing_page.css">
 	<link rel="stylesheet" type="text/css" href="../css/styles.css">
 
+	<title>Bintech - Inicio</title>
 
-	<title>Bintech - Servicios</title>
-	<style>
-		html,
-		body {
-			max-width: 100%;
-			overflow-x: hidden;
-		}
-	</style>
 </head>
 
 <body>
-	<?php require_once "cabeceras/header_movil2.php"; ?>
+	<?php require_once "cabeceras/header_movil.php"; ?>
+	<!-- ------------------------- SECCIÓN1 ------------------------- -->
+	<div class="main">
+		<section>
+			<img class="landing_page_background" src="../img/servicios/section1/servicios_sec1_background.png">
 
-	<img class="landing_page_background" src="../img/servicios/section1/servicios_sec1_background.png">
+			<img class="sec1_capa_4_movil" src="../img/landing_page/sec1_capa_4_movil.png">
 
-	<img class="sec1_capa_4_movil" src="../img/landing_page/sec1_capa_4_movil.png">
+			<h1 class="titulo_landing">servicios</h1>
 
-	<h1 class="titulo_landing">servicios</h1>
+			<div class="texto_landing">
+				<p>Nuestro compromiso es ayudar a nuestros clientes para que cuenten con modelos y procesos financieros de clase mundial, con la máxima eficiencia, de acuerdo a las características y necesidades propias de cada organización</p>
+			</div>
+			<?php require_once "../cabeceras/redes_sociales.php"; ?>
+		</section>
 
-	<div class="texto_landing">
-		<p>Nuestro compromiso es ayudar a nuestros clientes para que cuenten con modelos y procesos financieros de clase mundial, con la máxima eficiencia, de acuerdo a las características y necesidades propias de cada organización</p>
-	</div>
-	<?php require_once "../cabeceras/redes_sociales.php"; ?>
-	<div class="maintext_industria">
-		<div class="servicios_sec2_movil">
-		<h1 class="servicios_sec2_title">Mejoramiento de Rentabilidad <br>(Profitability Management)</h1>
+		<!-- ------------------------- SECCIÓN2 ------------------------- -->
+		<section>
+			<img class="sec2_capa_1" src="../img/index/section2/sec2_capa_1.png">
+			<img class="sec2_capa_2" src="../img/index/section2/sec2_capa_2.png">
+			<img class="sec2_capa_backgroud" src="../img/index/section2/sec2_backgroud.png">
+
+			<img class="sec2_capa_3" src="../img/index/section2/sec2_capa_3.png">
+
+			<h1 class="sec2_titulo">
+				Diferenciadores <br> y ventajas
+			</h1>
+
 			<div class="lista_dif">
 				<ul>
-					<li>Modelos de Rentabilidad</li>
-					<li>Administración Estratégica de Costos</li>
-					<li>Costo por servir</li>
-					<li>Estrategias de Precios y tarifas (Target Pricing)</li>
-					<li>Administración basada en valor (BVM)</li>
-					<li>Administración de la Capacidad</li>
-					<li>Costeo por Objetivo (Target Costing)</li>
-					<li>Administración del Desempeño (ABC/M,TDABC,etc.)</li>
+					<li>Soluciones estructuradas de acuerdo a las necesidades especificas de cada cliente.</li>
+					<br>
+					<li>Alto grado de especialización y enfoque 100% en soluciones de transformación financiera.</li>
+					<br>
+					<li>Alianzas estratégicas con empresas de tecnología líderes en la industria (SAP, Google, Oracle,
+						Microstrategy, Microsoft).</li>
 				</ul>
 			</div>
-			<img src="cabeceras/img_movil/servicios_sec2_background_movil.png" alt="">
-			
-		</div>
+
+			<h4 class="dif_200">+200<br><span>proyectos exitosos en diversos países e industrias</span></h4>
+		</section>
+
+		<!-- ------------------------- SECCIÓN2.5 ------------------------- -->
+		<section>
+			<img class="index_sec2_background" src="cabeceras/img_movil/fondo_index_sec2_5.png">
+
+			<h1 class="dif_title_2">
+				beneficios de nuestras soluciones
+			</h1>
+
+
+			<div class="brujula_contenedor">
+				<img class="icono_index_sec2" alt="" src="../img/index/section2/Icon-Brujula.png">
+				<h3 class="title_icono">Dirección financiera</h3>
+				<p class="text_icono">Transformación de la dirección financiera con un enfoque estratégico e innovador</p>
+			</div>
+
+			<div class="presentacion_contenedor">
+				<img class="icono_index_sec2" alt="" src="../img/index/section2/Icon-Presentacion.png">
+				<h3 class="title_icono">Incrementar el valor para los accionistas</h3>
+				<p class="text_icono">Hacer un mejor uso de la capacidad y los recursos, con el fin de incrementar el
+					valor y satisfacer a los empleados, clientes y accionistas</p>
+			</div>
+
+			<div class="engranes_contenedor">
+				<img class="icono_index_sec2" alt="" src="../img/index/section2/Icon-engranes.png">
+				<h3 class="title_icono">Optimización de procesos financieros</h3>
+				<p class="text_icono">Hacer un mejor uso de la capacidad y los recursos, con el fin de incrementar el
+					valor y satisfacer a los empleados, clientes y accionistas</p>
+			</div>
+		</section>
+
+		<!-- ------------------------- SECCIÓN3 ------------------------- -->
+		<section>
+			<!-- <img class="sec3_background" alt="" src="../img/index/section3/sec3_background.png"> -->
+			<img class="index_sec3_background" src="cabeceras/img_movil/fondo_index_sec2_5.png">
+
+			<h1 class="dif_title_2">
+				Experiencia<br>Certificada
+			</h1>
+
+			<p class="index_sec3_texto_movil">Somos la firma con más implantaciones de modelos de rentabilidad y costos, mediante el uso de las herramientas tecnológicas de SAP</p>
+
+			<img class="index_sec3_SAC" src="cabeceras/img_movil/SAP.png">
+			<img class="index_sec3_industrias" src="cabeceras/img_movil/Industrias_sec3.png">
+
+
+		</section>
+		<!-- ------------------------- SECCIÓN4 ------------------------- -->
+		<section>
+			<h1 class="sec4_titulo_blog">blog</h1>
+
+			<div class="contenedor_blog_principal">
+
+				<div class="contenedor_blog">
+					<img class="blog_img_1" src="../img/index/section4/blog_img_1.png">
+					<h3 class="titulo_contenedor_blog">¿Conoces sobre tu merma?</h3>
+					<p class="texto_contenedor_blog">Nuestro compromiso es ayudar a nuestros clientes para que cuenten con modelos y procesos financieros de clase mundial ...</p>
+					<button role="button" class="boton_blog_leer">Leer</button>
+				</div>
+
+				<div class="contenedor_blog">
+					<img class="blog_img_2" src="../img/index/section4/blog_img_2.png">
+					<h3 class="titulo_contenedor_blog">4 maneras de potenciar tu negocio</h3>
+					<p class="texto_contenedor_blog">Nuestro compromiso es ayudar a nuestros clientes para que cuenten con modelos y procesos financieros de clase mundial ...</p>
+					<button role="button" class="boton_blog_leer">Leer</button>
+				</div>
+
+				<div class="contenedor_blog">
+					<img class="blog_img_3" src="../img/index/section4/blog_img_3.png">
+					<h3 class="titulo_contenedor_blog">¿Por qué es necesaria una organización?</h3>
+					<p class="texto_contenedor_blog">Nuestro compromiso es ayudar a nuestros clientes para que cuenten con modelos y procesos financieros de clase mundial ...</p>
+					<button role="button" class="boton_blog_leer">Leer</button>
+				</div>
+
+				<div class="contenedor_blog">
+					<img class="blog_img_4" src="../img/index/section4/blog_img_4.png">
+					<h3 class="titulo_contenedor_blog">¿Qué tanto conoces tu negocio?</h3>
+					<p class="texto_contenedor_blog">Nuestro compromiso es ayudar a nuestros clientes para que cuenten con modelos y procesos financieros de clase mundial ...</p>
+					<button role="button" class="boton_blog_leer">Leer</button>
+				</div>
+
+			</div>
+		</section>
+		<!-- ------------------------- SECCIÓN5 (footer) ------------------------- -->
+		<?php require_once "cabeceras/footer_movil.php"; ?>
 
 	</div>
 
+	<script src="../js/main.js"></script>
 </body>
 
 </html>
